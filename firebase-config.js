59373722d732c66717735a1fe33505e268531c1a
Fwehd0101 <zzwfwehd@gmail.com>
2025-05-04 03:43:38 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDB8NoMrvM9dSyND1CASAFImysYYkIuJFQ",
+  authDomain: "xo-game-98dd9.firebaseapp.com",
+  databaseURL: "https://xo-game-98dd9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "xo-game-98dd9",
+  storageBucket: "xo-game-98dd9.appspot.com",
+  messagingSenderId: "682663768536",
+  appId: "1:682663768536:web:42c950c94d01ffc72d42ad"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
